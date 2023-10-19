ce30890f5079e5b8d7ee5e7d814d4f317498dc90
Edmig1 <124930647+Edmig1@users.noreply.github.com>
2023-10-19 16:57:47 -0300

    modulo funcionando e tela 2 com design fazido

--- Tela2.py
+++ Tela2.py
@@ -1,36 +1,24 @@
 import customtkinter as tk
 from modulo import *
-
+def enviar():
+    pass
 tk.set_appearance_mode("Light")
 tk.set_default_color_theme('themes/violet.json')
-class Produto:
-    def __init__(self,nome,preco,qtd):
-        self.nome = nome
-        self.preco = preco
-        self.qtd = int(qtd)
+janela = CriarJanela('Entrada de Produtos','854x480',2)
+framet2 = CriarFrame(janela,7,6,700,250)
+framet2.configure(corner_radius=25)
+framet2.configure(border_width=4,border_color='black')
+labelt2 = CriarLabel(framet2,'Entrada de Produtos',1,1,'black')
+labelt2.grid(columnspan=12)
+labelt2.configure(font=('Arial',27))
+inputt2_1 = CriarCaixaDeTexto(framet2,200,40,4,6,'Nome:')
+inputt2_1 = CriarCaixaDeTexto(framet2,200,40,5,6,'Preço:')
+inputt2_1 = CriarCaixaDeTexto(framet2,200,40,6,6,'Descrição:')
+inputt2_1 = CriarCaixaDeTexto(framet2,200,40,4,7,'Estoque:')
+inputt2_1 = CriarComboBox(framet2,200,40,['Camiseta','Short','Calças','Vestidos','Acessórios'],5,7)
+btnt1_2 = CriarBotão(framet2,'ENVIAR',enviar,6,7,200,40)
+btnt2_2 = CriarBotão(janela,'Controle De Estoque',enviar,11,6,200,40)
 
 produtos = []
 verificacao = []
-
-def enviar():
-    produto = Produto(input1.get(), input2.get(), input3.get())
-    produtos.append(produto)
-    if input1.get() in verificacao:
-        for i in range(len(produtos)):
-            if produtos[i].nome == input1.get():
-                print(produtos[i].nome)
-                print(produtos[i].qtd)
-                break
-        produtos.pop()
-        label1.configure(text='Produto Já Existente')
-        label1.configure(text_color='#f0bc11')
-    else:
-        verificacao.append(input1.get())
-        print(f'O número de itens cadastrados é: {len(produtos)}')
-        label1.configure(text='Produto Adicionado Com Sucesso')
-        label1.configure(text_color='lime')
-janela = CriarJanela('Entrada de Produtos','1200x800','')
-janela.iconbitmap('carreco.ico')
-
-
 janela.mainloop()
